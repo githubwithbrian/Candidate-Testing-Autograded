3dd0db24f93304976c7cfe69635d4ce090ff0c58
Brian Stephens <155034293+githubwithbrian@users.noreply.github.com>
2024-01-28 17:12:38 -0500

    candidateTestp2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,39 +3,45 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = ' '
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = ' ';
+let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+const questions = [
+  'Who was the first American woman in space? ',
+  'True or false: 5 kilometer == 5000 meters? ',
+  '(5 + 3)/2 * 10 = ? ', 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  'What is the minimum crew size for the ISS? '
+];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName = input.question("Enter your name: ");
+let candidateName = input.question("Enter your name ");
+console.log(candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let question = input.question("Who was the first American woman in space? ");
-let candidateAnswer = "Sally Ride";
-}
+  let candidateAnswers = input.question(questions[i]);
+  for (let i = 0; i < questions.length; i++) {
+    questions.push(candidateAnswers);
+    console.log(questions[i], candidateAnswers[i]);
+  }
+  
+  }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("Answer is correct.");
-} else { 
-  console.log("Answer is incorrect.");
-}
-
+console.log(`${candidateAnswers[i]}\n${correctAnswers[i]} `);
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
